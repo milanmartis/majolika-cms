24c5959c1f7ecf925ef2d06947ad3945ea183ab9
unknown <milanmartis@gmail.com>
2025-07-26 15:53:12 +0200

    vip13

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,5 @@
-import nodemailer from 'nodemailer';
 
-export default ({ env }: { env: (key: string, defaultValue?: any) => any }) => ({
+export default ({ env }: { env: (key: string, defaultValue?: any) => string }) => ({
   i18n: {
     enabled: true,
     config: {
@@ -9,38 +8,7 @@ export default ({ env }: { env: (key: string, defaultValue?: any) => any }) => (
     },
   },
 
-  email: {
-    config: {
-      provider: 'custom',   // <<< MUSÍ byť string, inak Strapi volá .toLowerCase()
-      providerOptions: {},  // prázdne, lebo si to riešime sami
-      settings: {
-        defaultFrom: env('EMAIL_DEFAULT_FROM', 'info@appdesign.sk'),
-        defaultReplyTo: env('EMAIL_REPLY_TO', 'info@appdesign.sk'),
-      },
-      // custom send handler
-      async send(options) {
-        const transporter = nodemailer.createTransport({
-          host: env('SMTP_HOST', 'smtp.m1.websupport.sk'),
-          port: Number(env('SMTP_PORT', 587)),
-          secure: false,
-          auth: {
-            user: env('SMTP_USER'),
-            pass: env('SMTP_PASS'),
-          },
-        });
-
-        await transporter.sendMail({
-          from: env('EMAIL_DEFAULT_FROM', 'info@appdesign.sk'),
-          replyTo: env('EMAIL_REPLY_TO', 'info@appdesign.sk'),
-          to: options.to,
-          subject: options.subject,
-          text: options.text,
-          html: options.html,
-        });
-      },
-    },
-  },
-
+  
   upload: {
     config: {
       provider: 'aws-s3',
