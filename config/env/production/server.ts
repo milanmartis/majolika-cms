8812c823fcff5fd8b25650adb14ddd300463e7cc
unknown <milanmartis@gmail.com>
2025-07-28 08:45:13 +0200

    vip35

--- config/env/production/server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import fs from 'fs';
-
-export default ({ env }: { env: any }) => ({
-  ssl: {
-    cert: fs.readFileSync(env('SSL_CERT_PATH')),
-    key:  fs.readFileSync(env('SSL_KEY_PATH')),
-  },
-});
