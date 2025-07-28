4bee0359a6e6eb06247653c3a3a052144d9e8d18
unknown <milanmartis@gmail.com>
2025-07-28 08:31:34 +0200

    vip33

--- config/server.ts
+++ config/server.ts
@@ -1,17 +1,28 @@
 import 'dotenv/config';
 import fs from 'fs';
 
-export default ({ env }: { env: any }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
-  proxy: true,
-  app: {
-    keys: env.array('APP_KEYS', []),
-  },
-  ssl: {
-    // Načítaj výhradne z ENV – nebude sa pokúšať o relatívne cesty
-    cert: fs.readFileSync(env('SSL_CERT_PATH')),
-    key:  fs.readFileSync(env('SSL_KEY_PATH')),
-  },
-});
+export default ({ env }: { env: any }) => {
+  // načítaj cesty z ENV
+  const certPath = env('SSL_CERT_PATH', null);
+  const keyPath  = env('SSL_KEY_PATH',  null);
+
+  // priprav ssl config, ak máme obe cesty
+  const sslConfig = (certPath && keyPath)
+    ? {
+        cert: fs.readFileSync(certPath),
+        key:  fs.readFileSync(keyPath),
+      }
+    : undefined;
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
+    proxy: true,
+    app: {
+      keys: env.array('APP_KEYS', []),
+    },
+    // pridáme ssl len ak máme cert + key
+    ...(sslConfig ? { ssl: sslConfig } : {}),
+  };
+};
