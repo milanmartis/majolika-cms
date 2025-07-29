b0f51a2780b8c5601e8f96dcf709f8e2f98725ea
unknown <milanmartis@gmail.com>
2025-07-29 13:26:28 +0200

    vip80

--- config/bootstrap.ts
+++ config/bootstrap.ts
@@ -1,7 +1,12 @@
+// config/bootstrap.ts
 import * as dotenv from 'dotenv';
-
 import * as path from 'path';
 
 export default async () => {
-  dotenv.config({ path: path.resolve('/home/ec2-user/majolika-cms/.env') });
+  const envPath = path.resolve('/home/ec2-user/majolika-cms/.env');
+  dotenv.config({ path: envPath });
+
+  console.log('✅ .env načítaný z:', envPath);
+  console.log('🔑 process.env.STRIPE_SECRET_KEY:', process.env.STRIPE_SECRET_KEY);
+  console.log('🌐 process.env.FRONTEND_URL:', process.env.FRONTEND_URL);
 };
