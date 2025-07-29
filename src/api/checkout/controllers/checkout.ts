5cff3a4248ee426f94e9c92ac172481b91452187
unknown <milanmartis@gmail.com>
2025-07-29 13:53:05 +0200

    vip81

--- src/api/checkout/controllers/checkout.ts
+++ src/api/checkout/controllers/checkout.ts
@@ -1,3 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: require('path').resolve(__dirname, '../../../../.env') });
+
+// alebo ak si v dist, môžeš to podmieniť buildom:
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: require('path').resolve(__dirname, '../../../../dist/.env') });
+}
+
 export default {
   async create(ctx) {
     console.log('CHECKOUT PAYLOAD:', ctx.request.body); // <- LOGNI SI DÁTA
