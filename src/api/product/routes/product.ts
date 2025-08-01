5113b4ed84411a4628d7032125369d2f1a5da86a
unknown <milanmartis@gmail.com>
2025-08-01 08:55:23 +0200

    vip123

--- src/api/product/routes/product.ts
+++ src/api/product/routes/product.ts
@@ -1,16 +1,5 @@
-import { factories } from '@strapi/strapi';
-
-
-const coreRouter = factories.createCoreRouter('api::product.product');
-
-const baseRoutes = typeof coreRouter.routes === 'function'
-  ? coreRouter.routes()
-  : coreRouter.routes;
-
 export default {
-  ...coreRouter,
   routes: [
-    ...baseRoutes,
     {
       method: 'GET',
       path: '/products/categories/:slug',
@@ -21,49 +10,4 @@ export default {
       },
     },
   ],
-} as const;
-// -----------------------------
-export const eventSessionRoutes = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/products/:id/event-sessions',
-      handler: 'product.eventSessions',
-      info: { type: 'content-api' },
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/products/:id/event-sessions/:sessionId/book',
-      handler: 'product.bookEventSession',
-      info: { type: 'content-api' },
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/products/:id/event-sessions/:sessionId/confirm-paid',
-      handler: 'product.confirmPaidBooking',
-      info: { type: 'content-api' },
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/products/:id/event-sessions/:sessionId/cancel/:bookingId',
-      handler: 'product.cancelBooking',
-      info: { type: 'content-api' },
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-} as const;
+};
