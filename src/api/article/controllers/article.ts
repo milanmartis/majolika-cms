e0104bdc8bf151b1201b3f5daf8a4eb4ad4fe718
unknown <milanmartis@gmail.com>
2025-08-04 17:56:44 +0200

    vip163

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -6,32 +6,24 @@ export default factories.createCoreController('api::article.article', ({ strapi 
     const { slug } = ctx.params;
     const locale = ctx.query.locale || 'en'; // default môže byť 'en'
 
-    // Nájdi všetky články so slug a locale
-    const articles = await strapi.entityService.findMany(
+    const [article] = await strapi.entityService.findMany(
       'api::article.article',
       {
         filters: { 
           slug,
-          locale
+          locale   // filtruj podľa jazyka
         },
         populate: {
           content: {
             populate: '*',
           },
         },
+        limit: 1,
       } as any
     );
 
-    if (articles.length === 0) {
-      return ctx.notFound('Article not found');
-    }
+    if (!article) return ctx.notFound('Article not found');
 
-    if (articles.length > 1) {
-      // Viacero rovnakých slug + locale nie je povolené
-      return ctx.badRequest('Multiple articles with the same slug and locale found');
-    }
-
-    // Presne jeden článok, vráť ho
-    return this.transformResponse(articles[0]);
+    return this.transformResponse(article);
   },
 }));
