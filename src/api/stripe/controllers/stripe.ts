f86c36668b3091dc0b1f9c58f844501c223f6e97
unknown <milanmartis@gmail.com>
2025-08-05 16:24:56 +0200

    vip178

--- src/api/stripe/controllers/stripe.ts
+++ src/api/stripe/controllers/stripe.ts
@@ -1,5 +1,29 @@
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
+
 export default {
-  async webhook(ctx) {
-    ctx.body = { hello: 'world' };
+  async webhook(ctx: any) {
+    console.log('WEBHOOK CONTROLLER CALLED!');
+    strapi.log.info('WEBHOOK CONTROLLER CALLED!');
+    const sig = ctx.request.headers['stripe-signature'];
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        ctx.req.body, // Buffer!
+        sig!,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
+    } catch (err: any) {
+      ctx.status = 400;
+      ctx.body = `Webhook Error: ${err.message}`;
+      return;
+    }
+
+    // Lognutie eventu, aby si videl že funguje
+    strapi.log.info('Stripe event:', event.type);
+
+    ctx.status = 200;
+    ctx.body = { received: true };
   }
 };
--- src/api/stripe/routes/stripe.ts
+++ src/api/stripe/routes/stripe.ts
@@ -2,21 +2,11 @@ export default {
   routes: [
     {
       method: 'POST',
-      path: '/stripe/webhook',       
+      path: '/stripe/webhook',
       handler: 'stripe.webhook',
       config: {
         auth: false,
         policies: [],
-        middlewares: [
-          async (ctx, next) => {
-            ctx.req.body = await new Promise(resolve => {
-              let data = '';
-              ctx.req.on('data', chunk => (data += chunk));
-              ctx.req.on('end', () => resolve(Buffer.from(data)));
-            });
-            await next();
-          },
-        ],
       },
     },
   ],
