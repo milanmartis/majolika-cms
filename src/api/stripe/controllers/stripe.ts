e6ed8445885ad540de66a80b164fefc5af8adca4
unknown <milanmartis@gmail.com>
2025-08-05 15:26:18 +0200

    vip17š

--- src/api/stripe/controllers/stripe.ts
+++ src/api/stripe/controllers/stripe.ts
@@ -1,8 +1,27 @@
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
+
 export default {
   async webhook(ctx: any) {
-    strapi.log.info('--- WEBHOOK CALLED ---');
+    const sig = ctx.request.headers['stripe-signature'];
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        ctx.req.body, // Buffer!
+        sig!,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
+    } catch (err: any) {
+      ctx.status = 400;
+      ctx.body = `Webhook Error: ${err.message}`;
+      return;
+    }
+
+    // Lognutie eventu, aby si videl že funguje
+    strapi.log.info('Stripe event:', event.type);
+
     ctx.status = 200;
-    ctx.body = { hello: 'world' };
-    return;
+    ctx.body = { received: true };
   }
 };
--- src/api/stripe/routes/stripe.ts
+++ src/api/stripe/routes/stripe.ts
@@ -7,6 +7,17 @@ export default {
       config: {
         auth: false,
         policies: [],
+        // Tento middleware spraví z req.body Buffer
+        middlewares: [
+          async (ctx, next) => {
+            ctx.req.body = await new Promise(resolve => {
+              let data = '';
+              ctx.req.on('data', chunk => (data += chunk));
+              ctx.req.on('end', () => resolve(Buffer.from(data)));
+            });
+            await next();
+          },
+        ],
       },
     },
   ],
