ade80dbbbb9d5c7d1ac3714d1cdcfca7b50eec92
unknown <milanmartis@gmail.com>
2025-08-05 17:33:37 +0200

    vip186

--- src/api/stripe/controllers/stripe.ts
+++ src/api/stripe/controllers/stripe.ts
@@ -4,20 +4,22 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
 export default {
   async webhook(ctx: any) {
     strapi.log.info('WEBHOOK CONTROLLER CALLED!');
-    strapi.log.info('typeof ctx.request.body', typeof ctx.request.body);
-    strapi.log.info('isBuffer', Buffer.isBuffer(ctx.request.body));
+    strapi.log.info('typeof body:', typeof ctx.request.body);
+    strapi.log.info('isBuffer:', Buffer.isBuffer(ctx.request.body));
+    strapi.log.info('body keys:', Object.keys(ctx.request.body));
     const sig = ctx.request.headers['stripe-signature'];
-    let event;
 
+    let event;
     try {
       event = stripe.webhooks.constructEvent(
-        ctx.request.body, // toto musí byť Buffer, ak je RAW middleware správne
+        ctx.request.body,
         sig!,
         process.env.STRIPE_WEBHOOK_SECRET!
       );
     } catch (err: any) {
       ctx.status = 400;
       ctx.body = `Webhook Error: ${err.message}`;
+      strapi.log.error(`Webhook Error: ${err.message}`);
       return;
     }
 
