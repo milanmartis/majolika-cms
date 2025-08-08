a221ef2b53fb2735463cf6aadb67873a9b970db2
unknown <milanmartis@gmail.com>
2025-08-08 10:37:41 +0200

    ped30

--- src/api/checkout/controllers/checkout.ts
+++ src/api/checkout/controllers/checkout.ts
@@ -1,7 +1,9 @@
 export default {
   async create(ctx) {
     const { body } = ctx.request;
-
+    const { items, customer, temporaryId } = ctx.request.body; // ← temporaryId TU
+    strapi.log.info('CHECKOUT PAYLOAD:');
+    strapi.log.info(JSON.stringify(ctx.request.body, null, 2));
     const result = await strapi.service('api::checkout.checkout').createSession(body);
     ctx.send(result);
   },
