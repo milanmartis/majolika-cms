7d50ad6f6097dd028a541eade0f19e82effbd433
unknown <milanmartis@gmail.com>
2025-08-19 18:57:26 +0200

    vgb19

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,16 @@
+// config/server.ts
 import 'dotenv/config';
 
 export default ({ env }: { env: any }) => ({
-  url: 'https://majolika-cms.appdesign.sk',
-  proxy: true,                 // DÔLEŽITÉ: aby ctx.secure čítal X-Forwarded-Proto
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
+  proxy: true,
+
+  // vypneme Strapi body parser pre všetky routy
+  router: {
+    bodyParser: false,
+  },
+
   app: { keys: env.array('APP_KEYS') },
 });
