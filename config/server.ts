3d16b3ee3a156505769e59984c1f8c09321ecfbd
unknown <milanmartis@gmail.com>
2025-08-19 07:39:25 +0200

    vgb9

--- config/server.ts
+++ config/server.ts
@@ -1,20 +1,23 @@
-// config/server.ts
 import 'dotenv/config';
 
 export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
-  proxy: { koa:true },
 
-  // vypneme Strapi body parser pre všetky routy
+  // verejná URL backendu
+  url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
+
+  // 🔑 toto nechaj ako boolean
+  proxy: true,
+
+  // ak naozaj chceš vypnúť bodyParser globálne
   router: {
     bodyParser: false,
   },
 
   app: {
     keys: env.array('APP_KEYS', [
-      'defaultKey1DefaultKey1DefaultKey1Def', 
+      'defaultKey1DefaultKey1DefaultKey1Def',
       'defaultKey2DefaultKey2DefaultKey2Def',
     ]),
   },
