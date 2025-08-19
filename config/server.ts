4be4a20724c58c5517b40ff5ec3dfe73b678de04
unknown <milanmartis@gmail.com>
2025-08-19 19:47:59 +0200

    vgb20

--- config/server.ts
+++ config/server.ts
@@ -4,13 +4,7 @@ import 'dotenv/config';
 export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'https://majolika-cms.appdesign.sk'),
-  proxy: true,
-
-  // vypneme Strapi body parser pre všetky routy
-  router: {
-    bodyParser: false,
-  },
-
-  app: { keys: env.array('APP_KEYS') },
+  url: 'https://majolika-cms.appdesign.sk', 
+  app: { keys: env.array('APP_KEYS') },     
+  proxy: { koa: true },                     
 });
