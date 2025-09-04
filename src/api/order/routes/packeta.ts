2fe578e18e1dcc2bf12f0dfa68e77c507f6c2c7d
unknown <milanmartis@gmail.com>
2025-09-04 16:58:59 +0200

    feat(post-office): integrate pickup widget9

--- src/api/order/routes/packeta.ts
+++ src/api/order/routes/packeta.ts
@@ -1,10 +1,10 @@
-export default {
-    routes: [
-      {
-        method: 'POST',
-        path: '/orders/:id/packeta/ship',
-        handler: 'order.shipPacketa',
-        config: { auth: { scope: ['admin'] } } // iba admin
-      }
-    ]
-  };
+// export default {
+//     routes: [
+//       {
+//         method: 'POST',
+//         path: '/orders/:id/packeta/ship',
+//         handler: 'order.shipPacketa',
+//         config: { auth: { scope: ['admin'] } } // iba admin
+//       }
+//     ]
+//   };
