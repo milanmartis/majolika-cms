6c00e52b4baf9284771c0a0d4e936f206c87c6f1
unknown <milanmartis@gmail.com>
2025-10-31 09:11:53 +0100

    v2025-10-31-3

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -10,7 +10,7 @@ type SendEmailArgs = {
 };
 
 export async function sendEmail({ to, subject, text, html, from, replyTo }: SendEmailArgs) {
-  const host = process.env.SMTP_HOST || "smtp.m1.websupport.sk";
+  const host = process.env.SMTP_HOST || "mail.webhouse.sk";
   const port = Number(process.env.SMTP_PORT) || 587;
   const user = process.env.SMTP_USER || "";
   const pass = process.env.SMTP_PASS || "";
