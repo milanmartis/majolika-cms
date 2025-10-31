994e07d5f81335a550ce800ef9db032c7772c53f
unknown <milanmartis@gmail.com>
2025-10-31 09:51:55 +0100

    v2025-10-31-7

--- src/api/relay/controllers/relay.ts
+++ src/api/relay/controllers/relay.ts
@@ -1,6 +1,6 @@
 export default {
     async go(ctx) {
-      const fe = 'https://staging.d2y68xwoabt006.amplifyapp.com/connect/google/redirect';
+      const fe = 'https://www.majolika.sk/connect/google/redirect';
       const { access_token, error } = ctx.query as Record<string, string | undefined>;
       if (error) return ctx.redirect(`${fe}#error=${encodeURIComponent(error)}`);
       if (!access_token) return ctx.redirect(`${fe}#error=missing_token`);
