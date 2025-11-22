f5836958c9dedce8c35afc24192caf3160a0e96b
unknown <milanmartis@gmail.com>
2025-11-22 23:34:39 +0100

    v2025-11-22-2

--- config/admin.ts
+++ config/admin.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
     sessions: {
-      accessTokenLifespan: 90,        // 15 min
-      idleRefreshTokenLifespan: 90, // 1 deň nečinnosti
-      maxRefreshTokenLifespan: 90,// 30 dní absolútne max
-      idleSessionLifespan: 90,       // 30 min nečinnosti
-      maxSessionLifespan: 90,     // 30 dní absolútne max
+      accessTokenLifespan: 900,        // 15 min
+      idleRefreshTokenLifespan: 86400, // 1 deň nečinnosti
+      maxRefreshTokenLifespan: 2592000,// 30 dní absolútne max
+      idleSessionLifespan: 1800,       // 30 min nečinnosti
+      maxSessionLifespan: 2592000,     // 30 dní absolútne max
     },
     cookie: {
       path: '/admin',
