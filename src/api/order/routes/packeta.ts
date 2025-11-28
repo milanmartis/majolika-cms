8a3233380c7b7c412f883dd0ce47e1db563ade4e
unknown <milanmartis@gmail.com>
2025-11-28 08:36:07 +0100

    2025-11-28-2

--- src/api/order/routes/packeta.ts
+++ src/api/order/routes/packeta.ts
@@ -1,12 +1,20 @@
 export default {
   routes: [
     {
+      method: 'GET',
+      path: '/orders/__ping_public',
+      handler: 'order.ping',
+      config: {
+        auth: false,
+      },
+    },
+    {
       method: 'POST',
       path: '/orders/:id/packeta/ship',
       handler: 'order.shipPacketa',
       config: {
-        // auth: false,  // toto nech je vypnuté
-        policies: ['admin::isAuthenticatedAdmin'], // admin guard
+        auth: false,                         // vypne users-permissions
+        policies: ['admin::isAuthenticatedAdmin'], // pustí len admina
       },
     },
   ],
