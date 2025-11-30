8825b4b9032370b92e20acd3f3ab3b6b4994046f
unknown <milanmartis@gmail.com>
2025-11-30 07:48:54 +0100

    v234

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -1,3 +1,82 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController(
+  'api::category.category',
+  ({ strapi }) => ({
+    async find(ctx) {
+      try {
+        ctx.query = {
+          ...ctx.query,
+          pagination: { pageSize: 1000 },
+          populate: ctx.query.populate ?? {
+            parent: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            children: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            products: {
+              fields: ['id'],
+            },
+          },
+        };
+
+        return await super.find(ctx);
+      } catch (err) {
+        strapi.log.error('Error in category.find', err);
+        // aspoň niečo v response:
+        ctx.throw(500, 'Error in category.find');
+      }
+    },
+
+    async findOne(ctx) {
+      try {
+        ctx.query = {
+          ...ctx.query,
+          populate: ctx.query.populate ?? {
+            parent: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            children: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            products: {
+              fields: ['id'],
+            },
+          },
+        };
+
+        return await super.findOne(ctx);
+      } catch (err) {
+        strapi.log.error('Error in category.findOne', err);
+        ctx.throw(500, 'Error in category.findOne');
+      }
+    },
+
+    async findRoots(ctx) {
+      try {
+        ctx.query = {
+          ...ctx.query,
+          pagination: { pageSize: 1000 },
+          filters: { parent: { id: { $null: true } } },
+          populate: ctx.query.populate ?? {
+            parent: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            children: {
+              fields: ['id', 'category_name', 'category_slug'],
+            },
+            products: {
+              fields: ['id'],
+            },
+          },
+        };
+
+        return await super.find(ctx);
+      } catch (err) {
+        strapi.log.error('Error in category.findRoots', err);
+        ctx.throw(500, 'Error in category.findRoots');
+      }
+    },
+  })
+);
